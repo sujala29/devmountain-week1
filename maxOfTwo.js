87b6240053ef58d635379bb471cd97ca5b97f401
sujala Potula <sujalapotula1@gmail.com>
2022-11-08 19:52:56 -0600

    first commit

---
+++ maxOfTwo.js
@@ -0,0 +1,29 @@
+let x = 5
+let y = 5
+
+// Write your code below this line.
+
+// if (x>y)
+// {
+//     console.log("x is Greater");
+// }
+// else
+// {
+// console.log("y is greater");
+// }
+
+// As an additional challenge, print out The values are identical. if x and y have the same value.
+
+if(x>y){
+    console.log("x is greater!");
+}
+else if(y>x)
+{
+console.log("y is greater");
+
+}
+else
+{console.log("x and y are equal!");
+}
+
+
